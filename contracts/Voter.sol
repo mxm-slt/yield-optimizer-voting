8102b46b9a24ea372c15f8c4cccec7949aa951c0
Alexey Kalmykov <alexey.kalmykoff@gmail.com>
2022-02-23 04:17:01 +0300

    downgrade to solidity 6, removed 2 unused contracts

--- contracts/Voter.sol
+++ contracts/Voter.sol
@@ -1,12 +1,18 @@
-pragma solidity 0.8.9;
+// SPDX-License-Identifier: MIT
+
+pragma solidity 0.6.12;
 
 import "@openzeppelin/contracts/access/Ownable.sol";
-import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+//// Conflict: IERC20 from OpenZepplilin with IERC20 from Boring (MiniChef import)
+import "@openzeppelin/contracts/token/ERC20/IERC20.sol"; 
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol"; 
+//import "@boringcrypto/boring-solidity/contracts/ERC20.sol";
+
 import "./MiniChefV2.sol";
 
 // Each voter has a corresponding ERC20 token to send to MiniChef
 interface IVoter {
-    function deposit(uint256 govTokenAmount);
+    function deposit(uint256 govTokenAmount) external;
 }
 
 contract Voter is IVoter, Ownable {
@@ -16,15 +22,15 @@ contract Voter is IVoter, Ownable {
     IERC20 public immutable govToken;
     IERC20 public immutable voteToken;
 
-    constructor(MiniChefV2 miniChef_, IERC20 govToken_, address rewardPool_) {
+    constructor(MiniChefV2 miniChef_, IERC20 govToken_, address rewardPool_) public {
         govToken = govToken_;
         miniChef = miniChef_;
         voteToken = new ERC20();
-        miniChef.add(?1?, voteToken, null);
+        //miniChef.add(1, voteToken, null);
     }
 
-    function deposit(uint256 govTokenAmount) {
-        miniChef.deposit(pid, govTokenAmount, rewardPool);
+    function deposit(uint256 govTokenAmount) external override {
+        miniChef.deposit(1, govTokenAmount, rewardPool);
     }
 
 }
