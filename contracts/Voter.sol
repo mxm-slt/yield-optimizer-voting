46a93efe5aa60d174db4a7e48c29b5b66160aae5
Maxim Sultakov <soultakov@gmail.com>
2022-03-08 15:06:22 +0300

    feat: pause/unpause

--- contracts/Voter.sol
+++ contracts/Voter.sol
@@ -7,16 +7,13 @@ import "@openzeppelin/contracts/math/SafeMath.sol";
 import "./MiniChefV2.sol";
 import "@openzeppelin/contracts/presets/ERC20PresetMinterPauser.sol";
 
-// TODO
-// 1. Use single pool inside mini chef
-// 2. Add integration test Voter -> Minichef -> RewardPool
-// 3. 
-
 // Each voter has a corresponding ERC20 token to send to MiniChef
 // Exchange rate between tokens is always 1:1, 1 GovToken == 1 VoteToken
 interface IVoter {
     event Vote(address indexed user, uint256 amount);
     event Unvote(address indexed user, uint256 amount);
+    event Paused();
+    event Unpaused();
 
     function vote(uint256 govTokenAmount) external;
 
@@ -42,15 +39,19 @@ contract Voter is IVoter, Ownable {
     uint256 public immutable chefPoolId;
 
     ERC20PresetMinterPauser immutable _voteToken;
+    uint private _totalTokenAmount;
     mapping(address => uint256) private _userVotes;
+    address[] private _users;
 
     uint8 constant DEFAULT_ALLOC_POINT = 1;
 
-    constructor(MiniChefV2 miniChef_, 
-                uint256 chefPoolId_, 
-                IERC20 govToken_, 
-                address rewardPool_, 
-                ERC20PresetMinterPauser voteToken_) public {
+    bool public active = true;
+
+    constructor(MiniChefV2 miniChef_,
+        uint256 chefPoolId_,
+        IERC20 govToken_,
+        address rewardPool_,
+        ERC20PresetMinterPauser voteToken_) public {
         govToken = govToken_;
         miniChef = miniChef_;
         _voteToken = voteToken_;
@@ -59,11 +60,12 @@ contract Voter is IVoter, Ownable {
     }
 
     function vote(uint256 tokenAmount) external override {
-        require(this.isActive(), "Voting is inactive");
+        require(this.isActive(), "Voting is paused");
         // transferring GovToken from user to this contract
         govToken.transferFrom(msg.sender, address(this), tokenAmount);
         // remember user vote count
         _userVotes[msg.sender] = _userVotes[msg.sender].add(tokenAmount);
+        _totalTokenAmount = _totalTokenAmount.add(tokenAmount);
         // mint and approve VoteToken transfer, the transfer will be done by miniChef
         _voteToken.mint(address(this), tokenAmount);
         _voteToken.approve(address(miniChef), tokenAmount);
@@ -75,10 +77,16 @@ contract Voter is IVoter, Ownable {
     function unvote(uint256 tokenAmount) external override {
         // decrease user vote count
         _userVotes[msg.sender] = _userVotes[msg.sender].sub(tokenAmount, "Not enough votes");
-        // withdraw VoteToken from minichef
-        miniChef.withdraw(chefPoolId, tokenAmount, address(this));
-        // burn withdrawn vote tokens
-        _voteToken.burn(tokenAmount);
+        _totalTokenAmount = _totalTokenAmount.sub(tokenAmount);
+
+        if (active) {
+            // withdraw VoteToken from minichef
+            miniChef.withdraw(chefPoolId, tokenAmount, address(this));
+            // burn withdrawn vote tokens
+            _voteToken.burn(tokenAmount);
+        } else {
+            // withdrawal and burning already done when pausing
+        }
         // send user their GovToken
         govToken.transfer(msg.sender, tokenAmount);
         emit Unvote(msg.sender, tokenAmount);
@@ -93,15 +101,28 @@ contract Voter is IVoter, Ownable {
     }
 
     function pause() external override onlyOwner {
-        //        _voteToken.pause();
+        require(this.isActive(), "Voting is already paused");
+        active = false;
+        if (_totalTokenAmount > 0) {
+            miniChef.withdrawAndHarvest(chefPoolId, _totalTokenAmount, rewardPoolAddress);
+            _voteToken.burn(_totalTokenAmount);
+        }
+        emit Paused();
     }
 
     function unpause() external override onlyOwner {
-        //        _voteToken.unpause();
+        require(!this.isActive(), "Voting is active");
+        active = true;
+        if (_totalTokenAmount > 0) {
+            _voteToken.mint(address(this), _totalTokenAmount);
+            _voteToken.approve(address(miniChef), _totalTokenAmount);
+            miniChef.deposit(chefPoolId, _totalTokenAmount, address(this));
+        }
+        emit Unpaused();
     }
 
     function isActive() external override returns (bool) {
-        return !_voteToken.paused();
+        return active;
     }
 
 }
